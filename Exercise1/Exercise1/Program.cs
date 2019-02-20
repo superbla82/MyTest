8388eb254c0408d8993d26eff20a0a93cdc3b898
superbla82 <47626216+superbla82@users.noreply.github.com>
2019-02-20 20:27:22 +0100

    Exercise 1 & 2

---
+++ Exercise1/Exercise1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Pointer
+{
+    unsafe static void Main()
+    {
+        int m = 300;
+        double fx = 300.600006;
+        char cht = 'z';
+
+        int* p = &m;
+        double* p2 = &fx;
+        char* p3 = &cht;
+
+        Console.WriteLine(m);
+        Console.WriteLine(fx);
+        Console.WriteLine(cht);
+
+        Console.WriteLine(*p);
+        Console.WriteLine((int)&m);
+
+        Console.WriteLine(*p2);
+        Console.WriteLine((int)&fx);
+
+        Console.WriteLine(*p3);
+        Console.WriteLine((int)&cht);
+    }
+}
---
+++ Exercise2/Exercise2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class MainClass
+    {
+        public static unsafe void Main(string[] args)
+        {
+            //int n = Console.Read();
+            int n = 5;
+            int* arr = stackalloc int[n];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = rnd.Next(0, 9);
+                Console.WriteLine($"Value at {i}: {arr[i]}");
+            }
+        }
+    }
+}
